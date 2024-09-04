e08a9c5adb21f79baca10b951cb9d2b8c580dde4
Abu Bakkar Siddique <abubakkar.mee@gmail.com>
2024-09-04 13:17:28 +0600

    add something

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
 
   return (
     <>
+      <h2>color generator</h2>
       <input type="color" value={color} onInput={(e) => setColor(e.target.value)} />
 
       <div className='container' style={{backgroundColor:color}}></div>
